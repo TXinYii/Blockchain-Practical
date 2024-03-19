e24550b65f3216d0811c4d6dbe8e4a29056d7ef8
TXinYii <tanxy-wm22@student.tarc.edu.my>
2024-03-19 09:13:14 +0800

    19/3/2024

--- lab5/exercise1 (child and parent)/child.sol
+++ lab5/exercise1 (child and parent)/child.sol
@@ -3,8 +3,19 @@ pragma solidity ^0.8.0;
 
 import "./parent.sol";
 
-contract Child is Parent{
-    function getValue() external view returns (uint){
-        return sum;
+contract Child{
+    Parent parentContract;
+
+    constructor(address _parentAddress){
+        parentContract = Parent(_parentAddress);
     }
+
+    function getValue() external view returns (uint){
+        return parentContract.sum();
+   }
+
+    function setValue(uint _A, uint _B) external{
+        parentContract.setValue(_A, _B);
+    }
+   
 }
--- lab5/exercise1 (child and parent)/parent.sol
+++ lab5/exercise1 (child and parent)/parent.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Parent{
+abstract contract Parent{
     uint internal sum;
-    function setValue(uint _A, uint _B) external{
-        sum = _A + _B;
+    function setValue(uint _A, uint _B) external virtual;
+    function getValue() external view returns (uint){
+        return sum;
     }
-    
 }
 
